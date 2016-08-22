12bf1fb346112a68f0dc51f3537ec85e938a1e9a
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-21 20:45:05 -0700

    main page markup and core init

--- ctdecide/js/decide/pages/main.js
+++ ctdecide/js/decide/pages/main.js
@@ -1,9 +1,11 @@
 CT.require("CT.all");
+CT.require("core");
 CT.require("user.core");
 CT.require("decide");
 if (!decide.core.util._user)
 	location = "/";
 
 CT.onload(function() {
-	decide.core.util.proposals();
+	CT.initCore();
+	decide.core.util.proposals("ctmain");
 });
