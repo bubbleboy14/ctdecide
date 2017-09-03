60be7f891414f8511501e563126d5a46320b37d6
bubbleboy14 <mario.balibrera@gmail.com>
2017-09-02 19:35:39 -0700

    main page: edit override()

--- ctdecide/js/decide/pages/main.js
+++ ctdecide/js/decide/pages/main.js
@@ -1,9 +1,11 @@
 CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
+CT.require("edit.core");
 CT.require("decide");
 
 CT.onload(function() {
 	CT.initCore();
 	decide.core.util.proposals("ctmain");
+	edit.core.override();
 });
