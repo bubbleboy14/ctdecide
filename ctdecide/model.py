a2750fc76674faaf44204f075e20e23cb0061f51
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-13 17:53:57 -0700

    model (Proposal, Objection, Vote): rmed ctuser kind constraint from user columns (also support ctuser subclasses)

--- ctdecide/model.py
+++ ctdecide/model.py
@@ -1,19 +1,19 @@
 from cantools import db
 
 class Proposal(db.TimeStampedBase):
-    user = db.ForeignKey(kind="ctuser")
+    user = db.ForeignKey()
     name = db.String()
     description = db.Text()
     final = db.Boolean(default=False)
 
 class Objection(db.TimeStampedBase):
-    user = db.ForeignKey(kind="ctuser")
+    user = db.ForeignKey()
     proposal = db.ForeignKey(kind=Proposal)
     name = db.String()
     description = db.Text()
     closed = db.Boolean(default=False)
 
 class Vote(db.TimeStampedBase):
-    user = db.ForeignKey(kind="ctuser")
+    user = db.ForeignKey()
     proposal = db.ForeignKey(kind=Proposal)
     position = db.Boolean()
