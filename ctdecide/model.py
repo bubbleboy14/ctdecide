98c2ac7356eabc7dad4e82a79cd8091f1b9a506a
bubbleboy14 <mario.balibrera@gmail.com>
2019-11-12 18:05:27 -0800

    moved passed(), vote(), count() from util to model.Proposal

--- ctdecide/model.py
+++ ctdecide/model.py
@@ -1,11 +1,55 @@
 from cantools import db
+from cantools import config
+from ctuser.model import CTUser, Conversation
+cfg = config.ctdecide
 
 class Proposal(db.TimeStampedBase):
     user = db.ForeignKey()
-    conversation = db.ForeignKey(kind="conversation") # from ctuser
+    conversation = db.ForeignKey(kind=Conversation)
     name = db.String()
     description = db.Text()
     final = db.Boolean(default=False)
+
+    def passed(self):
+        if self.final:
+            total_users = CTUser.query().count()
+            total_votes = Vote.query(Vote.proposal == self.key).count()
+            yes_votes = Vote.query(Vote.proposal == self.key,
+                Vote.position == True).count()
+            if total_votes / float(total_users) >= cfg.thresholds.participation:
+                if cfg.mode == "democracy":
+                    thresh = cfg.thresholds.majority
+                else: # consensus
+                    thresh = 1
+                    if Objection.query(Objection.proposal == self.key,
+                        Objection.closed == False).count():
+                        return False
+                return yes_votes / float(total_votes) >= thresh
+        return False
+
+    def vote(self, user, position):
+        existing = Vote.query(Vote.user == user,
+            Vote.proposal == self.key).get()
+        if existing:
+            from cantools.web import fail
+            fail("you already voted!")
+        Vote(user=user, proposal=self.key, position=position).put()
+        if self.passed():
+            pass # do whatever -- email peeps?
+
+    def count(self, user=None):
+        d = {
+            "yup": Vote.query(Vote.proposal == self.key,
+                Vote.position == True).count(),
+            "nope": Vote.query(Vote.proposal == self.key,
+                Vote.position == False).count()
+        }
+        if user:
+            uvote = Vote.query(Vote.user == user,
+                Vote.proposal == self.key).get()
+            if uvote:
+                d["user"] = uvote.position
+        return d
 
 class Objection(db.TimeStampedBase):
     user = db.ForeignKey()
